9e546a665afe81de83d58375fd8cf4109670ca34
Santosh Singh <ssingh@ntserv.doitbestcorp.com>
2026-08-01 16:16:50 +0530

    Create Abstraction on MediatR for CQRS - Command and Query separation - 2

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+// If not any return response
+public interface ICommandHandler<in TCommand>
+    : ICommandHandler<TCommand, Unit>
+    where TCommand : ICommand<Unit>
+{
+
+}
+
+// If response will not null
+public interface ICommandHandler<in TCommand, TResponse>
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull
+{
+}
