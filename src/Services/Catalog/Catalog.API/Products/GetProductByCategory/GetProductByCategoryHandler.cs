652c587233f149a05bec19768740da2fa84ec2f9
Santosh Singh <ssingh@ntserv.doitbestcorp.com>
2026-08-06 12:21:23 +0530

    Develop GetProductByCategory Handler with CQRS and MediatR

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string category)
+    : IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+internal class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Handling GetProductByCategoryQuery for category: {Category}", request.category);
+        var products = await session.Query<Product>()
+            .Where(p => p.Category.Contains(request.category))
+            .ToListAsync(cancellationToken);
+
+        return new GetProductByCategoryResult(products);
+    }
+}
